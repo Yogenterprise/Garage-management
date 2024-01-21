f153e65652b03514e7febc0bda8ab41c69830acd
salonipatell <120467387+salonipatell@users.noreply.github.com>
2024-01-21 14:40:35 +0530

    first commit

---
+++ garage_m/garage_m/doctype/job_cart/job_cart.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, saloni and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Job_cart', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ garage_m/garage_m/doctype/job_cart/test_job_cart.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, saloni and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJob_cart(FrappeTestCase):
+	pass
