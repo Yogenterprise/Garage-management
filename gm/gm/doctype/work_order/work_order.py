f87907ca91898751914622d18c02be4bcea5ee70
aazar <shaikhaazar11@gmail.com>
2024-01-25 18:30:00 +0530

    job cart workflow...az

--- gm/gm/doctype/work_order/work_order.py
+++ gm/gm/doctype/work_order/work_order.py
@@ -7,10 +7,27 @@ from frappe.model.document import Document
 class Workorder(Document):	
 	# pass
 	def validate(self):
-		if self.job_status == 1 and self.mechanic_remark != "":
-			frappe.msgprint("Good, can submit")
-			
+		# frappe.msgprint("After save executed")
+		if self.inspection_required == 1 and self.job_status == 1:
+			# frappe.msgprint("Good, can submit")
+			l = []
 			for row in self.get('inspection_details'):
+				l.append(row.parameter)
+			s = set(l)
+			if not (len(s) == len(l)):
+				frappe.throw("Inspection parameters are repeated...cannot create qc")
+
+			for row in self.get('inspection_details'):
+				e = frappe.db.exists({
+					"doctype" : "Quality Certificate",
+					"parameter":row.parameter,
+					"wo_reference":self.name,
+				})
+				if e:
+					frappe.msgprint(f"Document exists...!")
+					continue
+				frappe.msgprint("Creating Quality Certificate Document")
+
 				qc = frappe.get_doc({
 					"doctype":"Quality Certificate",
 					"job_card_reference": self.job_card_ref,
@@ -18,22 +35,47 @@ class Workorder(Document):
 					"parameter":row.parameter,
 					"parameter_description": row.parameter_description,
 					"acceptable_value":row.acceptable_value,					
-				})
+				})								
+				
+				qc.insert()	
+						
+		else:					
+			frappe.msgprint("Document Saved, Set inspection required to create inspection jobs.")
 
-				qc.insert()
-			
-			# all_status = True
-			# for row in self.get('inspection_details'):
+	def on_submit(self):
+		if self.job_status == 1 and self.inspection_required == 1 and self.mechanic_remark != "":
+			accepted = 0
+			total = 0; rows = 0
+			# frappe.throw("inside on submit")
+			for row in self.get('inspection_details'):
+				rows += 1
 				# row.inspection_status = "Accepted" if row.acceptable_value == row.parameter_reading else "Rejected"
-				# if row.acceptable_value == row.parameter_reading:
-				# 	row.inspection_status = "Accepted"
-				# 	frappe.msgprint(f"{row.inspection_status}")
-				# else:
-				# 	row.inspection_status = "Rejected"
-				# 	all_status = False
+				# frappe.throw(f"inside row...{row.inspection_status}")
+				if (row.inspection_status is not None) and (row.parameter_reading is not None):
+					total += 1
+					if (row.inspection_status.lower() == "accepted")  and (row.acceptable_value.lower() == row.parameter_reading.lower()):
+						# row.inspection_status = "Accepted"
+						accepted += 1
+						frappe.msgprint(f"{accepted}")
+					else:
+						frappe.msgprint(f"inside row...{row.inspection_status}")
+						
+				else:
+					frappe.throw("Status and reading required. cannot submit")
+					
+			average = accepted/total
+			self.average_rating = round(average,2)
+			frappe.db.set_value('Work order', self.name, 'average_rating', average)
+			if average < 0.5:
+				frappe.throw(f"Inspection not clear, cannot submit...{rows}...{total}")
+			
+			jc = frappe.get_doc('Job Cart', self.job_card_ref )
+			service_details_ct = jc.get('service_details')
+			for row in service_details_ct:
+				if row.work_ord_ref == self.name:
+					row.custom_service_status = "Completed"
+			jc.save()
 
-			# if not all_status:
-			# 	frappe.throw("All inspections are not clear, cannot submit")
-			# frappe.msgprint("Check box and mechanic remark present , can submit")			
-		else:			
-			frappe.throw("Set Remarks and Job Status to Submit.")
+			frappe.msgprint("Accuracy achieved , can submit")
+		else:					
+			frappe.throw("Set Remarks and Inspection required to submit.")
